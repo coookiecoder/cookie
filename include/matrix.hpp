9329583ef49c9dd9504d233cde1ff93040076d3e
cookie coder <ambroise.bareux@gmail.com>
2025-05-29 15:38:57 +0200

    added transpose (for testing lol)

--- include/matrix.hpp
+++ include/matrix.hpp
@@ -17,16 +17,43 @@ namespace cookie {
 
         public:
             Matrix() = default;
+            Matrix(const Matrix<Type> &other);
+            Matrix(Matrix<Type> &&other) noexcept ;
+
             Matrix(unsigned long row, unsigned long col);
             Matrix(std::initializer_list<std::initializer_list<Type>> value_list);
+
+            Matrix<Type> &operator=(const Matrix<Type> &other);
+            Matrix<Type> &operator=(Matrix<Type> &&other) noexcept ;
 
             [[nodiscard]] unsigned long row() const;
             [[nodiscard]] unsigned long col() const;
 
-            const Type* operator[](unsigned long row) const;
+            Type* operator[](unsigned long row) const;
 
             virtual ~Matrix();
     };
+
+    template <class Type>
+    Matrix<Type>::Matrix(const Matrix<Type>& other) : Matrix(other.row(), other.col()) {
+        for (int row = 0; row < other.row(); ++row) {
+            for (int col = 0; col < other.col(); ++col) {
+                this->_data[row][col] = other._data[row][col];
+            }
+        }
+    }
+
+    template <class Type>
+    Matrix<Type>::Matrix(Matrix<Type>&& other)  noexcept {
+        this->_data = other._data;
+        this->_row = other._row;
+        this->_col = other._col;
+
+        other._data = nullptr;
+        other._row = 0;
+        other._col = 0;
+    }
+
 
     template <class Type>
     Matrix<Type>::Matrix(const unsigned long row, const unsigned long col) : _row(row), _col(col) {
@@ -64,6 +91,37 @@ namespace cookie {
         }
     }
 
+    template <class Type>
+    Matrix<Type>& Matrix<Type>::operator=(const Matrix<Type>& other) {
+        if (&other == this)
+            return *this;
+
+        this->_data = new Type*[other.row()];
+
+        for (int new_row = 0; new_row < other.row(); ++new_row) {
+            this->_data[new_row] = new Type[other.col()];
+
+            for (int new_col = 0; new_col < other.col(); ++new_col) {
+                this->_data[new_row][new_col] = other[new_row][new_col];
+            }
+        }
+
+        return *this;
+    }
+
+    template <class Type>
+    Matrix<Type>& Matrix<Type>::operator=(Matrix<Type>&& other) noexcept {
+        this->_data = other._data;
+        this->_row = other._row;
+        this->_col = other._col;
+
+        other._data = nullptr;
+        other._row = 0;
+        other._col = 0;
+
+        return *this;
+    }
+
 
     template <class Type>
     unsigned long Matrix<Type>::row() const {
@@ -76,7 +134,7 @@ namespace cookie {
     }
 
     template <class Type>
-    const Type* Matrix<Type>::operator[](unsigned long row) const {
+    Type* Matrix<Type>::operator[](unsigned long row) const {
         return this->_data[row];
     }
 
@@ -106,7 +164,18 @@ namespace cookie {
 }
 
 namespace cookie {
+    template <class Type>
+    Matrix<Type> transpose(const Matrix<Type>& matrix) {
+        Matrix<Type> result(matrix.col(), matrix.row());
 
+        for (int row = 0; row < matrix.row(); ++row) {
+            for (int col = 0; col < matrix.col(); ++col) {
+                result[row][col] = matrix[col][row];
+            }
+        }
+
+        return result;
+    }
 }
 
 #include <matrix/matrix2D.hpp>
