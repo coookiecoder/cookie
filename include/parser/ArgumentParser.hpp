35ce53fbb1ea14d8f8fcd88d6d7d82df4e9302be
cookie coder <ambroise.bareux@gmail.com>
2026-03-11 09:04:56 +0100

    fix to namespace

--- include/parser/ArgumentParser.hpp
+++ include/parser/ArgumentParser.hpp
@@ -3,94 +3,96 @@
 #include <map>
 #include <string>
 
-class ArgumentParser {
-    public:
-        ArgumentParser() = default;
-        ~ArgumentParser() = default;
+namespace cookie {
+    class ArgumentParser {
+        public:
+            ArgumentParser() = default;
+            ~ArgumentParser() = default;
 
-        void parse(int argc, const char** argv);
+            void parse(int argc, const char** argv);
 
-        void add_needed_string(const std::string& key, std::string& value) {this->need_string.insert({key, value});}
+            void add_needed_string(const std::string& key, std::string& value) {this->need_string.insert({key, value});}
 
-        void add_needed_short(const std::string& key, short& value) {this->need_short.insert({key, value});}
-        void add_needed_int(const std::string& key, int& value) {this->need_int.insert({key, value});}
-        void add_needed_long(const std::string& key, long& value) {this->need_long.insert({key, value});}
-        void add_needed_long_long(const std::string& key, long long& value) {this->need_long_long.insert({key, value});}
+            void add_needed_short(const std::string& key, short& value) {this->need_short.insert({key, value});}
+            void add_needed_int(const std::string& key, int& value) {this->need_int.insert({key, value});}
+            void add_needed_long(const std::string& key, long& value) {this->need_long.insert({key, value});}
+            void add_needed_long_long(const std::string& key, long long& value) {this->need_long_long.insert({key, value});}
 
-        void add_needed_unsigned_short(const std::string& key, unsigned short& value) {this->need_unsigned_short.insert({key, value});}
-        void add_needed_unsigned_int(const std::string& key, unsigned int& value) {this->need_unsigned_int.insert({key, value});}
-        void add_needed_unsigned_long(const std::string& key, unsigned long& value) {this->need_unsigned_long.insert({key, value});}
-        void add_needed_unsigned_long_long(const std::string& key, unsigned long long& value) {this->need_unsigned_long_long.insert({key, value});}
+            void add_needed_unsigned_short(const std::string& key, unsigned short& value) {this->need_unsigned_short.insert({key, value});}
+            void add_needed_unsigned_int(const std::string& key, unsigned int& value) {this->need_unsigned_int.insert({key, value});}
+            void add_needed_unsigned_long(const std::string& key, unsigned long& value) {this->need_unsigned_long.insert({key, value});}
+            void add_needed_unsigned_long_long(const std::string& key, unsigned long long& value) {this->need_unsigned_long_long.insert({key, value});}
 
-        void add_needed_float(const std::string& key, float& value) {this->need_float.insert({key, value});}
-        void add_needed_double(const std::string& key, double& value) {this->need_double.insert({key, value});}
+            void add_needed_float(const std::string& key, float& value) {this->need_float.insert({key, value});}
+            void add_needed_double(const std::string& key, double& value) {this->need_double.insert({key, value});}
 
-        void add_needed_char(const std::string& key, char& value) {this->need_char.insert({key, value});}
-        void add_needed_unsigned_char(const std::string& key, unsigned char& value) {this->need_unsigned_char.insert({key, value});}
+            void add_needed_char(const std::string& key, char& value) {this->need_char.insert({key, value});}
+            void add_needed_unsigned_char(const std::string& key, unsigned char& value) {this->need_unsigned_char.insert({key, value});}
 
-        void add_needed_bool(const std::string& key, bool& value) {this->need_bool.insert({key, value});}
+            void add_needed_bool(const std::string& key, bool& value) {this->need_bool.insert({key, value});}
 
-        void add_optional_string(const std::string& key, std::string& value) {this->optional_string.insert({key, value});}
+            void add_optional_string(const std::string& key, std::string& value) {this->optional_string.insert({key, value});}
 
-        void add_optional_short(const std::string& key, short& value) {this->optional_short.insert({key, value});}
-        void add_optional_int(const std::string& key, int& value) {this->optional_int.insert({key, value});}
-        void add_optional_long(const std::string& key, long& value) {this->optional_long.insert({key, value});}
-        void add_optional_long_long(const std::string& key, long long& value) {this->optional_long_long.insert({key, value});}
+            void add_optional_short(const std::string& key, short& value) {this->optional_short.insert({key, value});}
+            void add_optional_int(const std::string& key, int& value) {this->optional_int.insert({key, value});}
+            void add_optional_long(const std::string& key, long& value) {this->optional_long.insert({key, value});}
+            void add_optional_long_long(const std::string& key, long long& value) {this->optional_long_long.insert({key, value});}
 
-        void add_optional_unsigned_short(const std::string& key, unsigned short& value) {this->optional_unsigned_short.insert({key, value});}
-        void add_optional_unsigned_int(const std::string& key, unsigned int& value) {this->optional_unsigned_int.insert({key, value});}
-        void add_optional_unsigned_long(const std::string& key, unsigned long& value) {this->optional_unsigned_long.insert({key, value});}
-        void add_optional_unsigned_long_long(const std::string& key, unsigned long long& value) {this->optional_unsigned_long_long.insert({key, value});}
+            void add_optional_unsigned_short(const std::string& key, unsigned short& value) {this->optional_unsigned_short.insert({key, value});}
+            void add_optional_unsigned_int(const std::string& key, unsigned int& value) {this->optional_unsigned_int.insert({key, value});}
+            void add_optional_unsigned_long(const std::string& key, unsigned long& value) {this->optional_unsigned_long.insert({key, value});}
+            void add_optional_unsigned_long_long(const std::string& key, unsigned long long& value) {this->optional_unsigned_long_long.insert({key, value});}
 
-        void add_optional_float(const std::string& key, float& value) {this->optional_float.insert({key, value});}
-        void add_optional_double(const std::string& key, double& value) {this->optional_double.insert({key, value});}
+            void add_optional_float(const std::string& key, float& value) {this->optional_float.insert({key, value});}
+            void add_optional_double(const std::string& key, double& value) {this->optional_double.insert({key, value});}
 
-        void add_optional_char(const std::string& key, char& value) {this->optional_char.insert({key, value});}
-        void add_optional_unsigned_char(const std::string& key, unsigned char& value) {this->optional_unsigned_char.insert({key, value});}
+            void add_optional_char(const std::string& key, char& value) {this->optional_char.insert({key, value});}
+            void add_optional_unsigned_char(const std::string& key, unsigned char& value) {this->optional_unsigned_char.insert({key, value});}
 
-        void add_optional_bool(const std::string& key, bool& value) {this->optional_bool.insert({key, value});}
+            void add_optional_bool(const std::string& key, bool& value) {this->optional_bool.insert({key, value});}
 
-    private:
-        bool parse_needed(const std::string& current, const std::string& next);
-        bool parse_optional(const std::string& current, const std::string& next);
+        private:
+            bool parse_needed(const std::string& current, const std::string& next);
+            bool parse_optional(const std::string& current, const std::string& next);
 
-        std::map<std::string, std::string&> need_string;
+            std::map<std::string, std::string&> need_string;
 
-        std::map<std::string, short&> need_short;
-        std::map<std::string, int&> need_int;
-        std::map<std::string, long&> need_long;
-        std::map<std::string, long long&> need_long_long;
+            std::map<std::string, short&> need_short;
+            std::map<std::string, int&> need_int;
+            std::map<std::string, long&> need_long;
+            std::map<std::string, long long&> need_long_long;
 
-        std::map<std::string, unsigned short&> need_unsigned_short;
-        std::map<std::string, unsigned int&> need_unsigned_int;
-        std::map<std::string, unsigned long&> need_unsigned_long;
-        std::map<std::string, unsigned long long&> need_unsigned_long_long;
+            std::map<std::string, unsigned short&> need_unsigned_short;
+            std::map<std::string, unsigned int&> need_unsigned_int;
+            std::map<std::string, unsigned long&> need_unsigned_long;
+            std::map<std::string, unsigned long long&> need_unsigned_long_long;
 
-        std::map<std::string, float&> need_float;
-        std::map<std::string, double&> need_double;
+            std::map<std::string, float&> need_float;
+            std::map<std::string, double&> need_double;
 
-        std::map<std::string, char&> need_char;
-        std::map<std::string, unsigned char&> need_unsigned_char;
+            std::map<std::string, char&> need_char;
+            std::map<std::string, unsigned char&> need_unsigned_char;
 
-        std::map<std::string, bool&> need_bool;
+            std::map<std::string, bool&> need_bool;
 
-        std::map<std::string, std::string&> optional_string;
+            std::map<std::string, std::string&> optional_string;
 
-        std::map<std::string, short&> optional_short;
-        std::map<std::string, int&> optional_int;
-        std::map<std::string, long&> optional_long;
-        std::map<std::string, long long&> optional_long_long;
+            std::map<std::string, short&> optional_short;
+            std::map<std::string, int&> optional_int;
+            std::map<std::string, long&> optional_long;
+            std::map<std::string, long long&> optional_long_long;
 
-        std::map<std::string, unsigned short&> optional_unsigned_short;
-        std::map<std::string, unsigned int&> optional_unsigned_int;
-        std::map<std::string, unsigned long&> optional_unsigned_long;
-        std::map<std::string, unsigned long long&> optional_unsigned_long_long;
+            std::map<std::string, unsigned short&> optional_unsigned_short;
+            std::map<std::string, unsigned int&> optional_unsigned_int;
+            std::map<std::string, unsigned long&> optional_unsigned_long;
+            std::map<std::string, unsigned long long&> optional_unsigned_long_long;
 
-        std::map<std::string, float&> optional_float;
-        std::map<std::string, double&> optional_double;
+            std::map<std::string, float&> optional_float;
+            std::map<std::string, double&> optional_double;
 
-        std::map<std::string, char&> optional_char;
-        std::map<std::string, unsigned char&> optional_unsigned_char;
+            std::map<std::string, char&> optional_char;
+            std::map<std::string, unsigned char&> optional_unsigned_char;
 
-        std::map<std::string, bool&> optional_bool;
-};
+            std::map<std::string, bool&> optional_bool;
+    };
+}
--- source/parser/ArgumentParser.cpp
+++ source/parser/ArgumentParser.cpp
@@ -1,55 +1,55 @@
 #include <parser/ArgumentParser.hpp>
 
-void ArgumentParser::parse(const int argc, const char** argv) {
+void cookie::ArgumentParser::parse(const int argc, const char** argv) {
     for (int i = 1; i < argc; i++) {
-        if (const auto bool_need_argument = need_bool.find(argv[i]); bool_need_argument != need_bool.end())
+        if (const auto bool_need_argument = this->need_bool.find(argv[i]); bool_need_argument != this->need_bool.end())
             bool_need_argument->second = true;
-        else if (const auto bool_optional_argument = optional_bool.find(argv[i]); bool_optional_argument != optional_bool.end())
+        else if (const auto bool_optional_argument = this->optional_bool.find(argv[i]); bool_optional_argument != this->optional_bool.end())
             bool_optional_argument->second = true;
         else if (i + 1 == argc)
             break;
-        else if (parse_needed(argv[i], argv[i + 1]))
+        else if (this->parse_needed(argv[i], argv[i + 1]))
             i++;
-        else if (parse_optional(argv[i], argv[i + 1]))
+        else if (this->parse_optional(argv[i], argv[i + 1]))
             i++;
     }
 }
 
-bool ArgumentParser::parse_needed(const std::string& current, const std::string& next) {
-    if (const auto string_argument = need_string.find(current); string_argument != need_string.end())
+bool cookie::ArgumentParser::parse_needed(const std::string& current, const std::string& next) {
+    if (const auto string_argument = this->need_string.find(current); string_argument != this->need_string.end())
         string_argument->second = next;
 
 
-    else if (const auto short_argument = need_short.find(current); short_argument != need_short.end())
+    else if (const auto short_argument = this->need_short.find(current); short_argument != this->need_short.end())
         short_argument->second = static_cast<short>(strtoll(next.c_str(), nullptr, 10));
 
-    else if (const auto int_argument = need_int.find(current); int_argument != need_int.end())
+    else if (const auto int_argument = this->need_int.find(current); int_argument != this->need_int.end())
         int_argument->second = static_cast<int>(strtoll(next.c_str(), nullptr, 10));
 
-    else if (const auto long_argument = need_long.find(current); long_argument != need_long.end())
+    else if (const auto long_argument = this->need_long.find(current); long_argument != this->need_long.end())
         long_argument->second = static_cast<long>(strtoll(next.c_str(), nullptr, 10));
 
-    else if (const auto long_long_argument = need_long_long.find(current); long_long_argument != need_long_long.end())
+    else if (const auto long_long_argument = this->need_long_long.find(current); long_long_argument != this->need_long_long.end())
         long_long_argument->second = strtoll(next.c_str(), nullptr, 10);
 
 
-    else if (const auto unsigned_short_argument = need_unsigned_short.find(current); unsigned_short_argument != need_unsigned_short.end())
+    else if (const auto unsigned_short_argument = this->need_unsigned_short.find(current); unsigned_short_argument != this->need_unsigned_short.end())
         unsigned_short_argument->second = static_cast<unsigned short>(strtoull(next.c_str(), nullptr, 10));
 
-    else if (const auto unsigned_int_argument = need_unsigned_int.find(current); unsigned_int_argument != need_unsigned_int.end())
+    else if (const auto unsigned_int_argument = this->need_unsigned_int.find(current); unsigned_int_argument != this->need_unsigned_int.end())
         unsigned_int_argument->second = static_cast<unsigned int>(strtoull(next.c_str(), nullptr, 10));
 
-    else if (const auto unsigned_long_argument = need_unsigned_long.find(current); unsigned_long_argument != need_unsigned_long.end())
+    else if (const auto unsigned_long_argument = this->need_unsigned_long.find(current); unsigned_long_argument != this->need_unsigned_long.end())
         unsigned_long_argument->second = static_cast<unsigned long>(strtoull(next.c_str(), nullptr, 10));
 
-    else if (const auto unsigned_long_long_argument = need_unsigned_long_long.find(current); unsigned_long_long_argument != need_unsigned_long_long.end())
+    else if (const auto unsigned_long_long_argument = this->need_unsigned_long_long.find(current); unsigned_long_long_argument != this->need_unsigned_long_long.end())
         unsigned_long_long_argument->second = strtoull(next.c_str(), nullptr, 10);
 
 
-    else if (const auto float_argument = need_float.find(current); float_argument != need_float.end())
+    else if (const auto float_argument = this->need_float.find(current); float_argument != this->need_float.end())
         float_argument->second = strtof(next.c_str(), nullptr);
 
-    else if (const auto double_argument = need_double.find(current); double_argument != need_double.end())
+    else if (const auto double_argument = this->need_double.find(current); double_argument != this->need_double.end())
         double_argument->second = strtod(next.c_str(), nullptr);
 
     else
@@ -57,41 +57,41 @@ bool ArgumentParser::parse_needed(const std::string& current, const std::string&
     return true;
 }
 
-bool ArgumentParser::parse_optional(const std::string& current, const std::string& next) {
-    if (const auto string_argument = optional_string.find(current); string_argument != optional_string.end())
+bool cookie::ArgumentParser::parse_optional(const std::string& current, const std::string& next) {
+    if (const auto string_argument = this->optional_string.find(current); string_argument != this->optional_string.end())
         string_argument->second = next;
 
 
-    else if (const auto short_argument = optional_short.find(current); short_argument != optional_short.end())
+    else if (const auto short_argument = this->optional_short.find(current); short_argument != this->optional_short.end())
         short_argument->second = static_cast<short>(strtoll(next.c_str(), nullptr, 10));
 
-    else if (const auto int_argument = optional_int.find(current); int_argument != optional_int.end())
+    else if (const auto int_argument = this->optional_int.find(current); int_argument != this->optional_int.end())
         int_argument->second = static_cast<int>(strtoll(next.c_str(), nullptr, 10));
 
-    else if (const auto long_argument = optional_long.find(current); long_argument != optional_long.end())
+    else if (const auto long_argument = this->optional_long.find(current); long_argument != this->optional_long.end())
         long_argument->second = static_cast<long>(strtoll(next.c_str(), nullptr, 10));
 
-    else if (const auto long_long_argument = optional_long_long.find(current); long_long_argument != optional_long_long.end())
+    else if (const auto long_long_argument = this->optional_long_long.find(current); long_long_argument != this->optional_long_long.end())
         long_long_argument->second = strtoll(next.c_str(), nullptr, 10);
 
 
-    else if (const auto unsigned_short_argument = optional_unsigned_short.find(current); unsigned_short_argument != optional_unsigned_short.end())
+    else if (const auto unsigned_short_argument = this->optional_unsigned_short.find(current); unsigned_short_argument != this->optional_unsigned_short.end())
         unsigned_short_argument->second = static_cast<unsigned short>(strtoull(next.c_str(), nullptr, 10));
 
-    else if (const auto unsigned_int_argument = optional_unsigned_int.find(current); unsigned_int_argument != optional_unsigned_int.end())
+    else if (const auto unsigned_int_argument = this->optional_unsigned_int.find(current); unsigned_int_argument != this->optional_unsigned_int.end())
         unsigned_int_argument->second = static_cast<unsigned int>(strtoull(next.c_str(), nullptr, 10));
 
-    else if (const auto unsigned_long_argument = optional_unsigned_long.find(current); unsigned_long_argument != optional_unsigned_long.end())
+    else if (const auto unsigned_long_argument = this->optional_unsigned_long.find(current); unsigned_long_argument != this->optional_unsigned_long.end())
         unsigned_long_argument->second = static_cast<unsigned long>(strtoull(next.c_str(), nullptr, 10));
 
-    else if (const auto unsigned_long_long_argument = optional_unsigned_long_long.find(current); unsigned_long_long_argument != optional_unsigned_long_long.end())
+    else if (const auto unsigned_long_long_argument = this->optional_unsigned_long_long.find(current); unsigned_long_long_argument != this->optional_unsigned_long_long.end())
         unsigned_long_long_argument->second = strtoull(next.c_str(), nullptr, 10);
 
 
-    else if (const auto float_argument = optional_float.find(current); float_argument != optional_float.end())
+    else if (const auto float_argument = this->optional_float.find(current); float_argument != this->optional_float.end())
         float_argument->second = strtof(next.c_str(), nullptr);
 
-    else if (const auto double_argument = optional_double.find(current); double_argument != optional_double.end())
+    else if (const auto double_argument = this->optional_double.find(current); double_argument != this->optional_double.end())
         double_argument->second = strtod(next.c_str(), nullptr);
 
     else
